9fcbef5e3613020d217dae30e3c48b2aaba6a9c6
apoorvapendse <apoorvavpendse@gmail.com>
2023-07-04 22:01:38 +0530

    tp

---
+++ basicio.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"enter the two numbers:\n";
+    int a,b;
+    cin>>a>>b;
+    cout<<"The sum of a and b is "<<a+b<<endl;;
+    return 0;
+}
---
+++ helloWorld.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <cstdlib>
+#include <unistd.h>
+using namespace std;
+int main(){
+    cout<<"hello world"<<endl;
+    int a = 12;
+    int b = 14;
+    cout<<"the sum of a+b is:"<<a+b<<endl;
+    
+    sleep(1);
+    system("clear");
+    cout<<"Cout makes use of insertion operator << whereas cin makes use of extraction operator >>"<<endl;
+
+
+}
